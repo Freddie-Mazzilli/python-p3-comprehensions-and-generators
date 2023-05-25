bf822d3757dabc32b25382196e076178ac8030d3
Freddie Mazzilli <freddie.mazzilli@gmail.com>
2023-05-25 11:59:16 -0400

    Completes comprehensions and generators lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    evens_list = []
+    for n in num_list:
+        if n % 2 == 0:
+            evens_list.append(n)
+    return(evens_list)
 
 def make_exclamation(sentence_list):
-    pass
+    exclamations = []
+    for s in sentence_list:
+        exclamations.append(f'{s}!')
+    return exclamations
